21cac8f67167eb87c661bb3d95150b103a2b2c8b
Connor Abbott <cwabbott0@gmail.com>
2021-07-05 18:32:42 +0200

    HACK: [dxgi] Implement DXGID3D10CreateDevice

--- src/dxgi/dxgi_main.cpp
+++ src/dxgi/dxgi_main.cpp
@@ -1,5 +1,6 @@
 #include "dxgi_factory.h"
 #include "dxgi_include.h"
+#include <d3dcommon.h>
 
 namespace dxvk {
   
@@ -33,6 +34,26 @@ extern "C" {
   
   DLLEXPORT HRESULT __stdcall CreateDXGIFactory(REFIID riid, void **ppFactory) {
     return dxvk::createDxgiFactory(0, riid, ppFactory);
+  }
+
+  typedef HRESULT __stdcall (*PFN_D3D11CoreCreateDevice)(
+            IDXGIFactory*       pFactory,
+            IDXGIAdapter*       pAdapter,
+            UINT                Flags,
+      const D3D_FEATURE_LEVEL*  pFeatureLevels,
+            UINT                FeatureLevels,
+            void**              ppDevice);
+
+  DLLEXPORT HRESULT __stdcall DXGID3D10CreateDevice(
+          HMODULE             d3d10core,
+          IDXGIFactory*       pFactory,
+          IDXGIAdapter*       pAdapter,
+          UINT                Flags,
+    const D3D_FEATURE_LEVEL*  pFeatureLevels,
+          UINT                FeatureLevels,
+          void**              ppDevice) {
+    PFN_D3D11CoreCreateDevice pD3D11CoreCreateDevice = (PFN_D3D11CoreCreateDevice)GetProcAddress(d3d10core, "D3D11CoreCreateDevice");
+    return pD3D11CoreCreateDevice(pFactory, pAdapter, Flags, pFeatureLevels, FeatureLevels, ppDevice);
   }
 
   DLLEXPORT HRESULT __stdcall DXGIDeclareAdapterRemovalSupport() {
